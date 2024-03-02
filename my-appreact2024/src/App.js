7afab36dee46bb5b7757be7257f9d814b467516d
Jonathan S.P <jonhnspa@gmail.com>
2024-03-02 16:57:54 +0100

    crear Componente resultado

--- my-appreact2024/src/App.js
+++ my-appreact2024/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Cabecera  from './Componentes/Cabecera';
 import Footer from './Componentes/Footer';
+import Resultado from './Componentes/Resultado';
 function App() {
   return (    
             < div>
             <Cabecera />
          
-       
+            <Resultado />
+
              <Footer />
             
             </div>
---
+++ my-appreact2024/src/Componentes/Resultado.js
@@ -0,0 +1,20 @@
+import './Resultado.css'
+
+function Resultado() {
+    return (    
+
+  <div className='resultado'>
+   <p>       
+    
+    El resultado es:
+
+
+    </p>
+
+  </div>         
+         
+      
+    );
+  }
+
+  export default  Resultado;
