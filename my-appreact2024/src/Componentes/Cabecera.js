4fba037abb0946905eac6d985697ec149dc7c634
Jonathan S.P <jonhnspa@gmail.com>
2024-03-02 15:13:05 +0100

    crear Componente cabecera

---
+++ my-appreact2024/src/Componentes/Cabecera.js
@@ -0,0 +1,20 @@
+import './Cabecera.css'
+
+function Cabecera() {
+    return (    
+
+  <div className='cabecera'>
+   <p>       
+    
+    Introduzca dos números
+
+
+    </p>
+
+  </div>         
+         
+      
+    );
+  }
+
+  export default  Cabecera;
